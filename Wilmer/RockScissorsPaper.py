da8fee5982c08c7fdc797483ac7d452b10c05bec
Henrik Sandström <mail@henriksandstrom.org>
2022-05-12 08:46:33 +0200

    Added project task

---
+++ Wilmer/RockScissorsPaper.py
@@ -0,0 +1,17 @@
+# INSTRUCTION:
+#
+# Create a Rock/Scissors/Paper game, where you play against the computer
+#    1. Print the options: 1=Rock, 2=Scissors, 3=Paper
+#    2. Read a digit (1, 2, 3) from the CONSOLE (representing Rock/Scissors/Paper)
+#    3. Check that no other digit than (1, 2, 3) is being entered.
+#       If a faulty value was added, print an error message and read a digit once again
+#    4. Calculate the computer choice (1, 2, 3) by using RANDOM function, and print it
+#    5. Calculate who won the round ('if' or 'case' clause)
+#    6. Print a WIN/LOOSE statement
+#
+#    Additional tasks if you have time:
+#    7. Handle multiple rounds (using a loop)
+#    8. The person that reaches 5 won rounds (check with 'if' clause) wins all over, and this shall be printed
+#
+# TIP: Do one point at the time and test that it works before moving on to the next point
+
